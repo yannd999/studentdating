2c8fb6428bb252369355bc111bae875e1680dc94
= <=>
2020-08-01 18:26:37 +0200

    main

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,26 @@
+package launch;
+
+import java.io.File;
+
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String webappDirLocation = "src/main/webapp/";
+
+        //The port that we should run on can be set into an environment variable
+        //Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if(webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
+        }
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClasses = new File("target/classes");
+    }
+}
